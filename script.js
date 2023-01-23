87acaa47439bb108f658494b1ce6f4b8e0814567
Marcilane Souza <91219079+MarcilaneSouza@users.noreply.github.com>
2023-01-22 23:09:49 -0300

    Add js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
- 
+function carregaDicionario() {
+
+    var biografias = [
+  
+      "Marielle Franco",
+  
+      "Rosa Parks",
+  
+      "Angela Davis",
+  
+      "Carolina de Jesus",
+  
+      "Lélia Gonzalez",
+  
+      "Conceição Evaristo",
+  
+    ];
+  
+    alert(biografias);
+  
+  }
+  
+  carregaDicionario();  
